bf681bfe6fe708b8bf19030a24cabb43e0415c93
Nadeem <n.adeem@outlook.com>
2020-08-29 19:37:05 +0530

    #7 Conditional Output

--- 6-create-react-app/my-app/src/IdCard.js
+++ 6-create-react-app/my-app/src/IdCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-function ninjas({ninjas}) {
+function ninjas({ ninjas }) {
 
     const ninjaList = ninjas.map(ninja => {
-        return (
+
+        return ninja.age > 30 ? (
             <div className="id-card" key={ninja.id}>
                 <div>name: {ninja.name}</div>
                 <div>age:{ninja.age}</div>
                 <div>Belt:{ninja.belt}</div>
             </div>
-        )
+        ) : null
+
+
     })
+
+
     return (
         <div className="ninja-list">
             {ninjaList}
