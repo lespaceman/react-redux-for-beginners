d2ca1d9fad1369357cd3ecb3c8a11b6d8cc784cf
Nadeem <n.adeem@outlook.com>
2020-08-30 22:06:26 +0530

    #19 deleteing data

--- 6-create-react-app/my-app/src/App.js
+++ 6-create-react-app/my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import IdCard from './Ninjas'
+import Ninjas from './Ninjas'
 import AddNinja from './AddNinja'
 
 class App extends Component {
@@ -10,6 +10,25 @@ class App extends Component {
       { name: "Yorki", age: "12", belt: "White", id: "3" }
     ]
   }
+
+  addNinja = (ninja) => {
+    console.log(ninja)
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,8 +36,8 @@ class App extends Component {
           <h1>
             Hello ya all
           </h1>
-          <IdCard ninjas={this.state.ninjas} />
-          <AddNinja />
+          <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+          <AddNinja addNinja={this.addNinja} />
         </header>
       </div>
     );
@@ -26,4 +45,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default App
--- 6-create-react-app/my-app/src/Ninjas.js
+++ 6-create-react-app/my-app/src/Ninjas.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
-function ninjas({ ninjas }) {
+function ninjas({ ninjas, deleteNinja }) {
 
     const ninjaList = ninjas.map(ninja => {
 
-        return ninja.age > 30 ? (
-            <div className="id-card" key={ninja.id}>
-                <div>name: {ninja.name}</div>
-                <div>age:{ninja.age}</div>
-                <div>Belt:{ninja.belt}</div>
-            </div>
-        ) : null
-
+        if (ninja.age < 30) {
+            return (
+                <div className="id-card" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age:{ninja.age}</div>
+                    <div>Belt:{ninja.belt}</div>
+                    <button onClick={() => { deleteNinja(ninja.id) }}>Delete</button>
+                </div>
+            )
+        } else {
+            return null
+        }
 
     })
-
 
     return (
         <div className="ninja-list">
