bfe4bc01cc45be23435a43f13d848b55698ae319
Nadeem <n.adeem@outlook.com>
2020-09-09 22:08:33 +0530

    Complete begginers code

--- 25-react-router/pokitimes/src/components/Post.js
+++ 25-react-router/pokitimes/src/components/Post.js
@@ -1,26 +1,19 @@
 import React, { Component } from "react";
-import axios from "axios"
+import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
-    state = {
-        post: null
-    }
-
-    componentDidMount() {
-        let id = this.props.match.params.post_id
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-            .then(res => {
-                this.setState({
-                    post: res.data
-                })
-                console.log(this.state)
-            })
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id)
+        this.props.history.push('/')
     }
     render() {
-        const post = this.state.post ? (
+        console.log(this.props)
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
+                <div className="center btn grey" onClick={this.handleClick}>Delete post</div>
             </div>
         ) : (
                 <div className="center">Loading post ..</div>
@@ -33,5 +26,19 @@ class Post extends Component {
         )
     }
 }
+const mapStateToProps = (state, ownProps) => {
+    let id = Number(ownProps.match.params.post_id)
 
-export default Post
+    return {
+        post: state.posts.find(post => post.id === id)
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch(deletePost(id)) }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
---
+++ 25-react-router/pokitimes/src/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+const initState = {
+    posts: [
+        { id: 1, title: "Dog laid an egg 🥚", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
+        { id: 2, title: "Parrot and cats are friends 🐈🦜", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
+        { id: 3, title: "Earth is round 🌏", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
+
+    ],
+}
+
+const rootReducer = (state = initState, action) => {
+    if (action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => { return action.id !== post.id })
+        return {
+            ...state,
+            posts: newPosts
+        }
+    }
+    return state
+}
+
+export default rootReducer
